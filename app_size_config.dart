bde9c1103354283b564899af0a437dcec71a6c4a
tarja77 <kemal1374@gmail.com>
2021-02-23 20:27:00 +0300

    Create app_size_config.dart

---
+++ app_size_config.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppSizeConfig {
+  MediaQueryData _mediaQueryData;
+  double screenWidth;
+  double screenHeight;
+  double blockSizeHorizontal;
+  double blockSizeVertical;
+  double _safeAreaHorizontal;
+  double _safeAreaVertical;
+  double safeBlockHorizontal;
+  double safeBlockVertical;
+
+  AppSizeConfig(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    blockSizeHorizontal = screenWidth / 100;
+    blockSizeVertical = screenHeight / 100;
+    _safeAreaHorizontal =
+        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+    _safeAreaVertical =
+        _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
+    safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
+    safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
+  }
+}
